8a9a4d2417e6d606629977996197ffe0e928bbe3
doihchiro <doihchiro@gmail.com>
2024-08-25 18:00:41 +0900

    문장 속 단어

---
+++ src/string/Ex03.java
@@ -0,0 +1,27 @@
+package string;
+
+import java.util.Scanner;
+
+public class Ex03 {
+
+    public String solution(String str) {
+        String answer = "";
+        int m = Integer.MIN_VALUE;
+        String[] s = str.split(" ");
+        for (String x : s) {
+            int len = x.length();
+            if (len > m) {
+                m = len;
+                answer = x;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Ex03 T = new Ex03();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(T.solution(str));
+    }
+}
