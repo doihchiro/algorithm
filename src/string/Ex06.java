ca6632581a2344d7608d233da656427ad63419c5
doihchiro <doihchiro@gmail.com>
2024-08-29 14:45:18 +0900

    중복 문자 제거

---
+++ src/string/Ex06.java
@@ -0,0 +1,25 @@
+package string;
+
+import java.util.Scanner;
+
+public class Ex06 {
+
+    public String solution(String str) {
+        String answer = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf(str.charAt(i)) == i) {
+                answer += str.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Ex06 ex05 = new Ex06();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(ex05.solution(str));
+    }
+}
